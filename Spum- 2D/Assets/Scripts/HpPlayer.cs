19b3d3d060ceca4f1141ed14cefe517d8ce597f0
tuquang123 <85723243+tuquang123@users.noreply.github.com>
2022-08-04 22:12:35 +0700

    Tuquang_10:11_Home_Add text popup

--- Spum- 2D/Assets/Scripts/HpPlayer.cs
+++ Spum- 2D/Assets/Scripts/HpPlayer.cs
@@ -9,8 +9,34 @@ public class HpPlayer : MonoBehaviour
     
     private static readonly int Dead = Animator.StringToHash("dead");
     private static readonly int Hurt = Animator.StringToHash("hurt");
+    IEnumerator DamageAnimation()
+    {
+        SpriteRenderer[] srs = GetComponentsInChildren<SpriteRenderer>();
+
+        for (int i = 0; i < 3; i++)
+        {
+            foreach (SpriteRenderer sr in srs)
+            {
+                Color c = sr.color;
+                c.a = 0;
+                sr.color = c;
+            }
+
+            yield return new WaitForSeconds(.1f);
+
+            foreach (SpriteRenderer sr in srs)
+            {
+                Color c = sr.color;
+                c.a = 1;
+                sr.color = c;
+            }
+
+            yield return new WaitForSeconds(.1f);
+        }
+    }
     public void TakeDamage(int damage)
     {
+        StartCoroutine(DamageAnimation());
         anm.SetBool(Hurt, true);
         health -= damage;
         Debug.Log("dameE");
@@ -18,7 +44,7 @@ public class HpPlayer : MonoBehaviour
         {
             Die();
         }
-        Invoke(nameof(NoHurt),0.3f);
+        Invoke(nameof(NoHurt),0.5f);
     }
     void NoHurt()
     {
