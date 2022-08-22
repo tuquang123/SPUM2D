7e789fd48fa795e6caa607bd237acc73f4d1c85e
Nguyen Le Hoang Quan <quannlh@gearinc.com>
2022-08-22 15:24:04 +0700

    modify shuriken

--- Spum- 2D/Assets/Scripts/Item/Shuriken/ShurikenSpawn.cs
+++ Spum- 2D/Assets/Scripts/Item/Shuriken/ShurikenSpawn.cs
@@ -5,21 +5,35 @@ using UnityEngine;
 public class ShurikenSpawn : MonoBehaviour
 {
     [SerializeField] private GameObject _player;
-    [SerializeField] private Shuriken _shurikenPref;
-    [SerializeField] private float _length = 3f;
-    
+    [SerializeField] private GameObject _shurikenPref; 
+    [SerializeField] private float _length;
+
 
     private void Start()
     {
-        
+        transform.position = _player.transform.position;
+        InitShuriken();
     }
 
-    public void InitShuriken(int count)
+    private void InitShuriken()
     {
+        var myAngleInDegrees = 360 /Inventory.Instance.suriken;
+        
         for (int i = 0; i < Inventory.Instance.suriken; i++)
         {
-            GameObject shuriken = Instantiate(_shurikenPref.gameObject);
+            var angle = myAngleInDegrees * i;
+            var sinOfAngle = Mathf.Sin((angle * Mathf.PI)/180);
+            var cosOfAngle = Mathf.Cos((angle * Mathf.PI)/180);
+            var pos = _player.transform.position + new Vector3(cosOfAngle * _length, sinOfAngle * _length, 0);
+            GameObject shuriken = Instantiate(_shurikenPref.gameObject, pos, Quaternion.identity, transform);
+
         }
+    }
+
+    public void AddShuriken()
+    {
+        Inventory.Instance.suriken++;
+        InitShuriken();
     }
         
 }
