c18e4f75b88f0ab9fdf9401cdb0c496e9e3aa0c1
tuquang <tulq@gearinc.com>
2022-09-12 15:05:54 +0700

    Update item drop

--- Spum- 2D/Assets/Scripts/Manager/Collision.cs
+++ Spum- 2D/Assets/Scripts/Manager/Collision.cs
@@ -7,8 +7,59 @@ public class Collision : MonoBehaviour
     //collision 
     private void OnTriggerEnter2D(Collider2D col)
     {
-        if (col.CompareTag("Gem"))
+        if (col.CompareTag("speed"))
         {
+            Inventory.Instance.speed++;
+            Destroy(col.gameObject);
+        }
+        if (col.CompareTag("speedAttack"))
+        {
+            Inventory.Instance.speedAttack++;
+            Destroy(col.gameObject);
+        }
+        if (col.CompareTag("drill"))
+        {
+            Inventory.Instance.drill++;
+            Destroy(col.gameObject);
+        }
+        if (col.CompareTag("rocket2"))
+        {
+            Inventory.Instance.rocket2++;
+            Destroy(col.gameObject);
+        }
+        if (col.CompareTag("shuriken"))
+        {
+            Inventory.Instance.shuriken++;
+            Destroy(col.gameObject);
+        }
+        if (col.CompareTag("radiate"))
+        {
+            Inventory.Instance.radiate++;
+            Destroy(col.gameObject);
+        }
+        if (col.CompareTag("speedAttackBasic"))
+        {
+            Inventory.Instance.speedAttackBasic++;
+            Destroy(col.gameObject);
+        }
+        if (col.CompareTag("speedAttackThunder"))
+        {
+            Inventory.Instance.speedAttackThunder++;
+            Destroy(col.gameObject);
+        }
+        if (col.CompareTag("AttackSpeedSaw"))
+        {
+            Inventory.Instance.attackSpeedSaw++;
+            Destroy(col.gameObject);
+        }
+        if (col.CompareTag("boom"))
+        {
+            Inventory.Instance.boom++;
+            Destroy(col.gameObject);
+        }
+        if (col.CompareTag("book"))
+        {
+            Inventory.Instance.book++;
             Destroy(col.gameObject);
         }
         if (col.CompareTag("Gold"))
--- Spum- 2D/Assets/Scripts/Manager/Inventory.cs
+++ Spum- 2D/Assets/Scripts/Manager/Inventory.cs
@@ -13,5 +13,5 @@ public class Inventory : Singleton<Inventory>
     public int gold ;
     public int kill;
     [Range(0, 4)]
-    public int speed, speedAttack, drill, rocket2, shuriken,thunder, radiate,speedAttackBasic, speedAttackThunder,attackSpeedSaw ,boom,book;
+    public int speed, speedAttack, drill, rocket2, shuriken, radiate,speedAttackBasic, speedAttackThunder,attackSpeedSaw ,boom,book;
 }
